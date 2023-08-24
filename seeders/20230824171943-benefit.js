334bf11408f0496ba946fecce24a4bf3bbeb98b5
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-08-25 01:21:50 +0800

    fix everything 1

---
+++ seeders/20230824171943-benefit.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert("Rewards", [
+      {
+        name: "Uang Rp.100.000",
+        point: "1000",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "1 Paket Sembako",
+        point: "1500",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
