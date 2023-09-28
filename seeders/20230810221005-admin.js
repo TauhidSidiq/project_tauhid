6c643bf62b9843cd04bd27920ef0e75f27a7bb8f
Rifki Andrean <rexarcorp23@gmail.com>
2023-09-28 14:16:36 +0800

    Lagi 2

--- seeders/20230810221005-admin.js
+++ seeders/20230810221005-admin.js
@@ -1,34 +1,34 @@
-// 'use strict';
-// const bcrypt = require("bcrypt")
+'use strict';
+const bcrypt = require("bcrypt")
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//     await queryInterface.bulkInsert("Admins", [
-//       {
-//         userName: "admin",
-//         password: bcrypt.hashSync("12345678", 10),
-//         createdAt: new Date(),
-//         updatedAt: new Date(),
-//       },
-//     ]);
-//   },
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert("Admins", [
+      {
+        userName: "admin",
+        password: bcrypt.hashSync("12345678", 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
 
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//   }
-// };
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
