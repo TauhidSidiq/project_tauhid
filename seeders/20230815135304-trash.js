048b3196710067d8cc89523859f08feedda2e3c3
Rifki Andrean <rexarcorp23@gmail.com>
2023-09-28 13:57:58 +0800

    Tambahan Di About dan hapus codingan

--- seeders/20230815135304-trash.js
+++ seeders/20230815135304-trash.js
@@ -1,57 +1,57 @@
-'use strict';
+// 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    await queryInterface.bulkInsert("Trashes", [
-      {
-        name: "Sampah Organik",
-        point: "1000",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Sampah Anorganik",
-        point: "2000",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Sampah Kertas",
-        point: "3000",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Sampah Residu",
-        point: "4000",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Sampah Bahan Berbahaya dan Beracun (B3)",
-        point: "5000",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//     await queryInterface.bulkInsert("Trashes", [
+//       {
+//         name: "Sampah Organik",
+//         point: "1000",
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//       {
+//         name: "Sampah Anorganik",
+//         point: "2000",
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//       {
+//         name: "Sampah Kertas",
+//         point: "3000",
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//       {
+//         name: "Sampah Residu",
+//         point: "4000",
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//       {
+//         name: "Sampah Bahan Berbahaya dan Beracun (B3)",
+//         point: "5000",
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//     ]);
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//   }
+// };
